93d07e76ac7c793cd039109ee3dac5271e2c8363
bijia <zhaobijia@gmail.com>
2022-11-04 16:15:54 -0400

    basic functions of ERC751 and ERC1155 abi encoding

--- Assets/SequenceSDK/SequenceSharp/Scripts/SequenceNethereum/ABIEncoding/ERC1155.cs
+++ Assets/SequenceSDK/SequenceSharp/Scripts/SequenceNethereum/ABIEncoding/ERC1155.cs
@@ -2,37 +2,86 @@ using System.Numerics;
 using System.Threading.Tasks;
 using System;
 
-public class ERC1155
+namespace SequenceSharp
 {
-    public static async Task<string> URI(BigInteger _)
+    public class ERC1155
     {
-        throw new NotImplementedException();
-    }
+        private static string abi = "[ { \"inputs\": [ { \"internalType\": \"string\", \"name\": \"uri_\", \"type\": \"string\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"constructor\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"account\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"bool\", \"name\": \"approved\", \"type\": \"bool\" } ], \"name\": \"ApprovalForAll\", \"type\": \"event\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"uint256[]\", \"name\": \"ids\", \"type\": \"uint256[]\" }, { \"indexed\": false, \"internalType\": \"uint256[]\", \"name\": \"values\", \"type\": \"uint256[]\" } ], \"name\": \"TransferBatch\", \"type\": \"event\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"uint256\", \"name\": \"id\", \"type\": \"uint256\" }, { \"indexed\": false, \"internalType\": \"uint256\", \"name\": \"value\", \"type\": \"uint256\" } ], \"name\": \"TransferSingle\", \"type\": \"event\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": false, \"internalType\": \"string\", \"name\": \"value\", \"type\": \"string\" }, { \"indexed\": true, \"internalType\": \"uint256\", \"name\": \"id\", \"type\": \"uint256\" } ], \"name\": \"URI\", \"type\": \"event\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"account\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"id\", \"type\": \"uint256\" } ], \"name\": \"balanceOf\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address[]\", \"name\": \"accounts\", \"type\": \"address[]\" }, { \"internalType\": \"uint256[]\", \"name\": \"ids\", \"type\": \"uint256[]\" } ], \"name\": \"balanceOfBatch\", \"outputs\": [ { \"internalType\": \"uint256[]\", \"name\": \"\", \"type\": \"uint256[]\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"account\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" } ], \"name\": \"isApprovedForAll\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"_address\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"_amount\", \"type\": \"uint256\" } ], \"name\": \"ownerMint\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256[]\", \"name\": \"ids\", \"type\": \"uint256[]\" }, { \"internalType\": \"uint256[]\", \"name\": \"amounts\", \"type\": \"uint256[]\" }, { \"internalType\": \"bytes\", \"name\": \"data\", \"type\": \"bytes\" } ], \"name\": \"safeBatchTransferFrom\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"id\", \"type\": \"uint256\" }, { \"internalType\": \"uint256\", \"name\": \"amount\", \"type\": \"uint256\" }, { \"internalType\": \"bytes\", \"name\": \"data\", \"type\": \"bytes\" } ], \"name\": \"safeTransferFrom\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" }, { \"internalType\": \"bool\", \"name\": \"approved\", \"type\": \"bool\" } ], \"name\": \"setApprovalForAll\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"bytes4\", \"name\": \"interfaceId\", \"type\": \"bytes4\" } ], \"name\": \"supportsInterface\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"name\": \"uri\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" } ]";
 
-    public static async Task BalanceOf<BigInteger>(string account, BigInteger id)
-    {
-        throw new NotImplementedException();
-    }
+        public readonly static Wallet _wallet;
+        public static async Task<string> URI(BigInteger _id, string address, string abi)
+        {
+            //throw new NotImplementedException();
+            string URI = await _wallet.ExecuteSequenceJS(@"
+                const provider = wallet.getProvider()!
+                const erc1155 = new ethers.Contract(" + address + @", " + abi + @", provider);
+                var URI = await erc1155.URI(" + _id+ @");
+                return URI;
 
-    public static async Task<BigInteger[]> BalanceOfBatch(string[] accounts, BigInteger[] ids)
-    {
-        throw new NotImplementedException();
-    }
+            ");
 
-    public static async Task SetApprovalForAll(string operatorAddress, bool approved)
-    {
-        throw new NotImplementedException();
-    }
-    public static async Task<bool> IsApprovedForAll(string account, string operatorAddress)
-    {
-        throw new NotImplementedException();
-    }
-    public static async Task SafeTransferFrom(string from, string to, BigInteger id, BigInteger amount, string data)
-    {
-        throw new NotImplementedException();
-    }
-    public static async Task SafeBatchTransferFrom(string from, string to, BigInteger[] ids, BigInteger[] amounts, string data)
-    {
-        throw new NotImplementedException();
+            return URI;
+        }
+
+        public static async Task BalanceOf<BigInteger>(string account, BigInteger id, string address, string abi)
+        {
+            // throw new NotImplementedException();
+            string balanceOf = await _wallet.ExecuteSequenceJS(@"
+                const provider = wallet.getProvider()!
+                const erc1155 = new ethers.Contract(" + address + @", " + abi + @", provider);
+                var balanceOf = await erc1155.balanceOf(" + account+","+ id + @");
+                return balanceOf;
+
+            ");
+        }
+
+        public static async Task<BigInteger[]> BalanceOfBatch(string[] accounts, BigInteger[] ids, string address, string abi)
+        {
+            //throw new NotImplementedException();
+            string accountsJS = "[";
+            foreach(var acc in accounts)
+            {
+                accountsJS += acc +",";
+            }
+            accountsJS = accountsJS.Remove(accountsJS.Length - 1, 1);
+            accountsJS += "]";
+
+            string idsJS = "[";
+            foreach (var i in idsJS)
+            {
+                idsJS += i + ",";
+            }
+            idsJS = idsJS.Remove(idsJS.Length - 1, 1);
+            idsJS += "]";
+
+
+            var balanceOfBatchRes = await _wallet.ExecuteSequenceJS(@"
+                const provider = wallet.getProvider()!
+                const erc1155 = new ethers.Contract(" + address + @", " + abi + @", provider);
+                var balanceOfBatch = await erc1155.balanceOfBatch(" + accountsJS + "," + idsJS + @");
+                return balanceOfBatch;
+
+            ");
+            //TODO: parse string to array of bigInteger
+            BigInteger[] balanceOfBatch = null;
+            return balanceOfBatch;
+        }
+
+        public static async Task SetApprovalForAll(string operatorAddress, bool approved)
+        {
+            throw new NotImplementedException();
+        }
+        public static async Task<bool> IsApprovedForAll(string account, string operatorAddress)
+        {
+            throw new NotImplementedException();
+        }
+        public static async Task SafeTransferFrom(string from, string to, BigInteger id, BigInteger amount, string data)
+        {
+            throw new NotImplementedException();
+        }
+        public static async Task SafeBatchTransferFrom(string from, string to, BigInteger[] ids, BigInteger[] amounts, string data)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Assets/SequenceSDK/SequenceSharp/Scripts/SequenceNethereum/ABIEncoding/ERC721.cs
+++ Assets/SequenceSDK/SequenceSharp/Scripts/SequenceNethereum/ABIEncoding/ERC721.cs
@@ -1,62 +1,108 @@
 using System.Numerics;
 using System.Threading.Tasks;
 using System;
-public class ERC721
+namespace SequenceSharp
 {
+    public class ERC721
+    {
+        private static string abi = "[ { \"inputs\": [ { \"internalType\": \"string\", \"name\": \"name_\", \"type\": \"string\" }, { \"internalType\": \"string\", \"name\": \"symbol_\", \"type\": \"string\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"constructor\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"approved\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"Approval\", \"type\": \"event\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"bool\", \"name\": \"approved\", \"type\": \"bool\" } ], \"name\": \"ApprovalForAll\", \"type\": \"event\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"Transfer\", \"type\": \"event\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"approve\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" } ], \"name\": \"balanceOf\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"getApproved\", \"outputs\": [ { \"internalType\": \"address\", \"name\": \"\", \"type\": \"address\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" } ], \"name\": \"isApprovedForAll\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"name\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"ownerOf\", \"outputs\": [ { \"internalType\": \"address\", \"name\": \"\", \"type\": \"address\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"safeTransferFrom\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" }, { \"internalType\": \"bytes\", \"name\": \"_data\", \"type\": \"bytes\" } ], \"name\": \"safeTransferFrom\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" }, { \"internalType\": \"bool\", \"name\": \"approved\", \"type\": \"bool\" } ], \"name\": \"setApprovalForAll\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"bytes4\", \"name\": \"interfaceId\", \"type\": \"bytes4\" } ], \"name\": \"supportsInterface\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"symbol\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"tokenURI\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"transferFrom\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" } ]";
 
-    public static async Task<string> Name()
-    {
-        throw new NotImplementedException();
-    }
+        public readonly static Wallet _wallet;
+        public static async Task<string> Name(string address, string abi)
+        {
+            //throw new NotImplementedException();
+            string name = await _wallet.ExecuteSequenceJS(@"
+                const provider = wallet.getProvider()!
+                const erc721 = new ethers.Contract(" + address + @", " + abi + @", provider);
+                var name = await erc721.name();
+                return name;
 
-    public static async Task<string> Symbol()
-    {
-        throw new NotImplementedException();
-    }
+            ");
 
-    public static async Task TokenURI(BigInteger tokenId)
-    {
-        throw new NotImplementedException();
-    }
-    public static async Task BalanceOf(string owner)
-    {
-        throw new NotImplementedException();
-    }
+            return name;
 
-    public static async Task OwnerOf(BigInteger tokenId)
-    {
-        throw new NotImplementedException();
-    }
+        }
 
-    public static async Task SafeTransferFrom(string from, string to, BigInteger tokenId)
-    {
-        throw new NotImplementedException();
-    }
+        public static async Task<string> Symbol(string address, string abi)
+        {
+            //throw new NotImplementedException();
+            string symbol = await _wallet.ExecuteSequenceJS(@"
+                const provider = wallet.getProvider()!
+                const erc721 = new ethers.Contract(" + address + @", " + abi + @", provider);
+                var symbol = await erc721.symbol();
+                return symbol;
 
-    public static async Task TransferFrom(string from, string to, BigInteger tokenId)
-    {
-        throw new NotImplementedException();
-    }
-    public static async Task Approve(string to, BigInteger tokenId)
-    {
-        throw new NotImplementedException();
-    }
-    public static async Task GetApproved(BigInteger tokenId)
-    {
-        throw new NotImplementedException();
-    }
-    public static async Task SetApprovalForAll(string operatorAddress,bool _approved)
-    {
-        throw new NotImplementedException();
-    }
+            ");
+            return symbol;
+        }
 
-    public static async Task IsApprovedForAll(string owner, string operatorAddress)
-    {
-        throw new NotImplementedException();
-    }
+        public static async Task<string> TokenURI(BigInteger tokenId, string address, string abi)
+        {
+            //throw new NotImplementedException();
+            string tokenURI = await _wallet.ExecuteSequenceJS(@"
+                const provider = wallet.getProvider()!
+                const erc721 = new ethers.Contract(" + address + @", " + abi + @", provider);
+                var tokenURI = await erc721.tokenURI("+tokenId+@");
+                return tokenURI;
 
-    public static async Task SafeTransferFrom(string from, string to, BigInteger tokenId, string data)
-    {
-        throw new NotImplementedException();
+            ");
+
+            return tokenURI;
+        }
+        public static async Task<BigInteger> BalanceOf(string owner, string address, string abi)
+        {
+            //throw new NotImplementedException();
+            var balanceOf = BigInteger.Parse(await _wallet.ExecuteSequenceJS(@"
+                const provider = wallet.getProvider()!
+                const signer = wallet.getSigner();
+                const erc721 = new ethers.Contract(" + address + @", " + abi + @", provider);
+                var balanceOf = await erc721.balanceOf(signer.getAddress()||" + owner + @");
+            "));
+            return balanceOf;
+        }
+
+        public static async Task OwnerOf(BigInteger tokenId, string address, string abi)
+        {
+            //throw new NotImplementedException();
+            string ownerOf = await _wallet.ExecuteSequenceJS(@"
+                const provider = wallet.getProvider()!
+                const erc721 = new ethers.Contract(" + address + @", " + abi + @", provider);
+                var ownerOf = await erc721.ownerOf(" + tokenId + @");
+                return ownerOf;
+
+            ");
+        }
+
+        public static async Task SafeTransferFrom(string from, string to, BigInteger tokenId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task TransferFrom(string from, string to, BigInteger tokenId)
+        {
+            throw new NotImplementedException();
+        }
+        public static async Task Approve(string to, BigInteger tokenId)
+        {
+            throw new NotImplementedException();
+        }
+        public static async Task GetApproved(BigInteger tokenId)
+        {
+            throw new NotImplementedException();
+        }
+        public static async Task SetApprovalForAll(string operatorAddress, bool _approved)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task IsApprovedForAll(string owner, string operatorAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task SafeTransferFrom(string from, string to, BigInteger tokenId, string data)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
