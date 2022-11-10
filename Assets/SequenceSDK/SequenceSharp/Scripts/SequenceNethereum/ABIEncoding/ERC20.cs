5a56c078e59a888d8bd842ce23d0aaeea7e8c094
bijia <zhaobijia@gmail.com>
2022-11-10 16:06:23 -0500

    erc20 -> Nethereum calls

--- Assets/SequenceSDK/SequenceSharp/Scripts/SequenceNethereum/ABIEncoding/ERC20.cs
+++ Assets/SequenceSDK/SequenceSharp/Scripts/SequenceNethereum/ABIEncoding/ERC20.cs
@@ -24,26 +24,16 @@ namespace SequenceSharp
 
     public class ERC20 : MonoBehaviour
     {
-      /*  private readonly static string abi = @"[
-    // Read-Only Functions
-    'function name() view returns (string)',
-    'function balanceOf(address owner) view returns (uint256)',
-    'function decimals() view returns (uint8)',
-    'function symbol() view returns (string)',
-    'function totalSupply() view returns (uint256)',
-
-    // Authenticated Functions
-    'function transfer(address to, uint amount) returns (bool)',
-
-    
-]";*/
+      
       private readonly static string abi = "[ { \"inputs\": [ { \"internalType\": \"string\", \"name\": \"name_\", \"type\": \"string\" }, { \"internalType\": \"string\", \"name\": \"symbol_\", \"type\": \"string\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"constructor\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"uint256\", \"name\": \"value\", \"type\": \"uint256\" } ], \"name\": \"Approval\", \"type\": \"event\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"uint256\", \"name\": \"value\", \"type\": \"uint256\" } ], \"name\": \"Transfer\", \"type\": \"event\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" } ], \"name\": \"allowance\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"amount\", \"type\": \"uint256\" } ], \"name\": \"approve\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"account\", \"type\": \"address\" } ], \"name\": \"balanceOf\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"decimals\", \"outputs\": [ { \"internalType\": \"uint8\", \"name\": \"\", \"type\": \"uint8\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"subtractedValue\", \"type\": \"uint256\" } ], \"name\": \"decreaseAllowance\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"spender\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"addedValue\", \"type\": \"uint256\" } ], \"name\": \"increaseAllowance\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"name\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"symbol\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"totalSupply\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"recipient\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"amount\", \"type\": \"uint256\" } ], \"name\": \"transfer\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"sender\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"recipient\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"amount\", \"type\": \"uint256\" } ], \"name\": \"transferFrom\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" } ]";
-        private static bool usingSequence = false;
-        private static Wallet _wallet;
 
+        private static Wallet _wallet = null;
+        private static Web3 web3 = null;
         private void Awake()
         {
             _wallet = FindObjectOfType<Wallet>();
+            web3 = new Web3();
+            web3.Client.OverridingRequestInterceptor = new SequenceInterceptor(_wallet);
         }
 
         /// <summary>
@@ -52,40 +42,16 @@ namespace SequenceSharp
         /// <param name="address">Contract address</param>
         /// <param name="chainId"></param>
         /// <returns></returns>
-        public static async Task<string> Name(string address, int chainId)
+        public static async Task<string> Name(string address)
         {
-
-            
-            if (usingSequence)
-            {
-                string name = await _wallet.ExecuteSequenceJS(@"
-                
-                const wallet = seq.getWallet();           
-                const networks = await wallet.getNetworks();
-                const n = networks.find(n => n['chainId']==" + chainId + @");
-                const signer = wallet.getSigner(n);
-                const abi =" + abi + @";
-                const erc20 = new ethers.Contract('" + address + @"', abi, signer);               
-                var name = await erc20.name();
-                return name;"
-
-                );
-
-                return name;
-            }
-            else
-            {
-                var web3 = new Web3();
-
-                web3.Client.OverridingRequestInterceptor = new SequenceInterceptor(_wallet);
-                var nameOfFunctionMessage = new NameFunction() { };
+                         
                 var contract = web3.Eth.GetContract(abi, address);
                 var nameFunction = contract.GetFunction("name");
                 var name = await nameFunction.CallAsync<string>();
-                Debug.Log("result : "+ name);
+                
                 
                 return name;
-            }
+            
         }
 
         /// <summary>
@@ -94,41 +60,14 @@ namespace SequenceSharp
         /// <param name="address">Contract address</param>
         /// <param name="chainId"></param>
         /// <returns></returns>
-        public static async Task<string> Symbol(string address, int chainId)
+        public static async Task<string> Symbol(string address)
         {
-            if (usingSequence)
-            {
-                //throw new NotImplementedException();
-                string symbol = await _wallet.ExecuteSequenceJS(@"
-                const wallet = seq.getWallet();           
-                const networks = await wallet.getNetworks();
-                const n = networks.find(n => n['chainId']==" + chainId + @");
-                const signer = wallet.getSigner(n);
-                const abi =" + abi + @";
-                const erc20 = new ethers.Contract('" + address + @"', abi, signer);  
+            var contract = web3.Eth.GetContract(abi, address);
+            var symbolFunction = contract.GetFunction("symbol");
+            var symbol = await symbolFunction.CallAsync<string>();
 
-                var symbol = await erc20.symbol();
-                return symbol;
-            ");
-                return symbol;
-            }
-            else
-            {
-                /*var web3 = new Web3();
 
-                web3.Client.OverridingRequestInterceptor = new SequenceInterceptor(_wallet);
-                var symbolFunctionMessage = new SymbolFunction() { };
-                *//*var abiEncode = new ABIEncode();
-                var result = abiEncode.GetSha3ABIParamsEncodedPacked(nameOfFunctionMessage);
-                Debug.Log("from abiEncode: " + result.ToHex());*//*
-                var symbolHandler = web3.Eth.GetContractQueryHandler<SymbolFunction>();
-                Debug.Log(symbolHandler.ToString());
-                var symbol_ = await symbolHandler.QueryAsync<string>(address, symbolFunctionMessage);
-                Debug.Log(symbol_);
-                //return name_;
-                return null;*/
-                return null;
-            }
+            return symbol;
         }
 
 
@@ -139,20 +78,12 @@ namespace SequenceSharp
         /// <param name="address">Contract address</param>
         /// <param name="chainId"></param>
         /// <returns></returns>
-        public static async Task<BigInteger> Decimals(string address, int chainId)
+        public static async Task<BigInteger> Decimals(string address)
         {
-            //throw new NotImplementedException();
-            var decimals = BigInteger.Parse(await _wallet.ExecuteSequenceJS(@"
-                const wallet = seq.getWallet();           
-                const networks = await wallet.getNetworks();
-                const n = networks.find(n => n['chainId']==" + chainId + @");
-                const signer = wallet.getSigner(n);
-                const abi =" + abi + @";
-                const erc20 = new ethers.Contract('" + address + @"', abi, signer);  
-
-                var decimals = await erc20.decimals();
-                return decimals;
-            "));
+            var contract = web3.Eth.GetContract(abi, address);
+            var decimalsFunction = contract.GetFunction("decimals");
+            var decimals = await decimalsFunction.CallAsync<BigInteger>();
+           
             return decimals;
         }
 
@@ -162,24 +93,16 @@ namespace SequenceSharp
         /// <param name="address">Contract address</param>
         /// <param name="chainId"></param>
         /// <returns></returns>
-        public static async Task<BigInteger> TotalSupply(string address, int chainId)
+        public static async Task<BigInteger> TotalSupply(string address)
         {
-            //throw new NotImplementedException();
-            var totalSupply = await _wallet.ExecuteSequenceJS(@"
-                const wallet = seq.getWallet();           
-                const networks = await wallet.getNetworks();
-                const n = networks.find(n => n['chainId']==" + chainId + @");
-                const signer = wallet.getSigner(n);
-                const abi =" + abi + @";
-                const erc20 = new ethers.Contract('" + address + @"', abi, signer);  
+            var contract = web3.Eth.GetContract(abi, address);
+            var totalSupplyFunction = contract.GetFunction("totalSupply");
+            var totalSupply = await totalSupplyFunction.CallAsync<BigInteger>();
 
-                var totalSupply = await erc20.totalSupply();
-                console.log(totalSupply);
-                return totalSupply;
-            ");
-            ERC20Supply totalSupplyParsed = JsonConvert.DeserializeObject<ERC20Supply>(totalSupply);
-            return BigInteger.Parse(totalSupplyParsed.hex.Substring(2), System.Globalization.NumberStyles.HexNumber); ;
+            return totalSupply;
         }
+
+    
 
         /// <summary>
         /// Returns the amount of tokens owned by account.
@@ -188,7 +111,7 @@ namespace SequenceSharp
         /// <param name="chainId"></param>
         /// /// <param name="account">Account address, if not provided, it will be the account address from sequence wallet </param>
         /// <returns></returns>
-        public static async Task<BigInteger> BalanceOf(string address, int chainId, string account = null)
+        public static async Task<BigInteger> BalanceOf(string address,  string account = null)
         {
             if(account == null)
             {
@@ -196,22 +119,11 @@ namespace SequenceSharp
 
                 account = await _wallet.GetAddress();
             }
-            //throw new NotImplementedException();
-            var balanceOf = await _wallet.ExecuteSequenceJS(@"
-                const wallet = seq.getWallet();           
-                const networks = await wallet.getNetworks();
-                const n = networks.find(n => n['chainId']==" + chainId + @");
-                const signer = wallet.getSigner(n);
-                const abi =" + abi + @";
-                const erc20 = new ethers.Contract('" + address + @"', abi, signer);  
+            var contract = web3.Eth.GetContract(abi, address);
+            var balanceOfFunction = contract.GetFunction("balanceOf");
+            var balanceOf = await balanceOfFunction.CallAsync<BigInteger>(address);
 
-                var balanceOf = await erc20.balanceOf('" + account+@"');
-                console.log(balanceOf);
-                return balanceOf;
-            ");
-            ERC20Balance balanceOfParsed = JsonConvert.DeserializeObject<ERC20Balance>(balanceOf);
-
-            return BigInteger.Parse(balanceOfParsed.hex.Substring(2), System.Globalization.NumberStyles.HexNumber);
+            return balanceOf;
             
         }
 
--- Assets/SequenceSDK/SequenceSharp/Scripts/SequenceNethereum/ABIEncoding/ERC20Example.cs
+++ Assets/SequenceSDK/SequenceSharp/Scripts/SequenceNethereum/ABIEncoding/ERC20Example.cs
@@ -7,7 +7,6 @@ namespace SequenceSharp
     public class ERC20Example : MonoBehaviour
     {
         //Set in inspector
-        public int chainId = 137;
         public string contractAddress = "";
         public string accountAddress = "";
         // Start is called before the first frame update
@@ -19,15 +18,15 @@ namespace SequenceSharp
         public async void ERC20Examples()
         {
             Debug.Log("[Sequence] ERC20 Token Example:");
-            var name = await ERC20.Name(contractAddress, chainId);
+            var name = await ERC20.Name(contractAddress);
             Debug.Log("name: " + name);
-            var symbol = await ERC20.Symbol(contractAddress, chainId);
+            var symbol = await ERC20.Symbol(contractAddress);
             Debug.Log("symbol: " + symbol);
-            var decimals = await ERC20.Decimals(contractAddress, chainId);
+            var decimals = await ERC20.Decimals(contractAddress);
             Debug.Log("decimals: " + decimals);
-            var totalSupply = await ERC20.TotalSupply(contractAddress, chainId);
+            var totalSupply = await ERC20.TotalSupply(contractAddress);
             Debug.Log("totalSupply: " + totalSupply);
-            var balanceOf = await ERC20.BalanceOf(contractAddress, chainId, accountAddress);
+            var balanceOf = await ERC20.BalanceOf(contractAddress, accountAddress);
             Debug.Log("balanceOf: " + balanceOf);
         }
 
